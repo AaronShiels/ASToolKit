e49649b8c16380eb3fde57134b13c003bd931a03
Aaron Shiels <aaron.shiels@outlook.com>
2014-09-12 22:29:35 +1000

    Fixing some errors

---
+++ ToolKit.Web/Controllers/RoutineBuilderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToolKit.Web.Controllers
+{
+    public class RoutineBuilderController : Controller
+    {
+        // GET: RoutineBuilder
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
