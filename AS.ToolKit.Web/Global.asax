9013d3a765f5653ed9b4f144a4e4125294416348
Aaron Shiels <aaron.shiels@outlook.com>
2014-09-12 19:34:49 +1000

    A new beginning

--- AS.ToolKit.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AS.ToolKit.Web.MvcApplication" Language="C#" %>
---
+++ ToolKit.Web/Controllers/BudgetTrackerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToolKit.Web.Controllers
+{
+    public class BudgetTrackerController : Controller
+    {
+        // GET: BudgetTracker
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
