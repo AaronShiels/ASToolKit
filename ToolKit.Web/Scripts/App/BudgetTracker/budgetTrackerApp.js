2ca0878cd9154719abc7173daaa9a8ace5e68e14
Aaron Shiels <aaron.shiels@outlook.com>
2014-09-14 12:30:27 +1000

    Beginnings of Angular BudgetTracker app

---
+++ ToolKit.Web/Scripts/App/BudgetTracker/budgetTrackerApp.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('budgetTrackerApp', []);
+
+app.value('Frequencies', {
+    weekly: 'weekly',
+    fortnightly: 'fortnightly',
+    monthly: 'monthly',
+    halfYearly: 'halfYearly',
+    yearly: 'yearly'
+});
+
+app.factory('Bill', function (Frequencies) {
+    function Bill(name, amount, frequency, approxNextDueDate) {
+        this.name = name;
+        this.amount = amount;
+        this.frequency = frequency;
+        this.nextDue = approxNextDueDate;
+    }
+
+    return (Bill);
+});
+
+app.factory('Income', function (Frequencies) {
+    function Income(name, amount, frequency, approxNextDueDate) {
+        this.name = name;
+        this.amount = amount;
+        this.frequency = frequency;
+        this.nextDue = approxNextDueDate;
+    }
+
+    return (Income);
+});
---
+++ ToolKit.Web/Scripts/App/BudgetTracker/budgetTrackerController.js
@@ -0,0 +1,11 @@
+﻿app.controller('budgetTrackerController', function ($scope, Income, Bill, Frequencies) {
+    $scope.today = new Date();
+    $scope.incomes = [
+        new Income('Collection House Ltd', 800, Frequencies.fortnightly, new Date(2014,09,17))
+    ];
+    $scope.bills = [
+        new Bill('Phone',70, Frequencies.monthly, new Date(2014,9,20)),
+        new Bill('Rent',150, Frequencies.weekly, new Date(2014,9,16))
+    ];
+});
+
